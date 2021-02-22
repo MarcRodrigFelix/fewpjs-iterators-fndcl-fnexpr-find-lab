b0c14b946fb1310f0b79aa372c56d1457eb8d798
Marcos <m.rodrig.felix@gmail.com>
2021-02-21 18:51:49 -0800

    commit tree 8a842cb6d071d5455bff91420af3365b47e4aa9c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
+
+
+
+function superbowlWin(arrayOfObjects){
+  const superbowl = arrayOfObjects.find( obj => obj.result === "W")
+
+  return superbowl ? superbowl.year : undefined
+};
